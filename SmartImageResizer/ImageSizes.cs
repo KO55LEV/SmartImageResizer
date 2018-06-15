72a99403c4581aa30c3dd7d77fbb157149877616
Andrei Koshelev <andrei.koshelev@uk.pwc.com>
2018-06-15 16:27:04 +0100

    Added Azure API resizer

---
+++ SmartImageResizer/ImageSizes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartImageResizer
+{
+    public class ImageSizes
+    {
+        public ImageSizes()
+        {
+            Sizes = new List<ImageSize>
+            {
+                new ImageSize() {Width = 960, Height = 600},
+                new ImageSize() {Width = 960, Height = 400},
+                new ImageSize() {Width = 960, Height = 300},
+                new ImageSize() {Width = 800, Height = 600},
+                new ImageSize() {Width = 400, Height = 300},
+                new ImageSize() {Width = 631, Height = 427},
+                new ImageSize() {Width = 250, Height = 250},
+                new ImageSize() {Width = 150, Height = 250},
+                new ImageSize() {Width = 50, Height = 50},
+                new ImageSize() {Width = 30, Height = 30}
+            };
+
+        }
+
+        public List<ImageSize> Sizes { get; set; }
+    }
+
+    public class ImageSize
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
--- SmartImageResizer/Program.cs
+++ SmartImageResizer/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace SmartImageResizer
 {
     class Program
     {
 
+        public static Byte[] AzureResize(Stream myBlob, int width, int height, bool smartCrop)
+        {
+            bool smartCropping = smartCrop;
+            string _apiKey = "yourAzureKey";
+            string _apiUrlBase = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/generateThumbnail";
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(_apiUrlBase);
+                httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
+                using (HttpContent content = new StreamContent(myBlob))
+                {
+                    //get response
+                    content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/octet-stream");
+                    var uri = $"{_apiUrlBase}?width={width}&height={height}&smartCropping={smartCropping.ToString()}";
+                    var response = httpClient.PostAsync(uri, content).Result;
+                    var responseBytes = response.Content.ReadAsByteArrayAsync().Result;
+
+                    return responseBytes;
+                }
+            }
+        }
+
         //ImageSmartResizer wrapper 
 
         static void Main(string[] args)
         {
+
+            //path to the original image 
+            string fileToResize = @"c:\temp\demo.jpg";
+
+            //Folder where resized images will be uploaded 
+            string rootOutputFolder = @"c:\temp\ImageResize";
+
             
+            if (!Directory.Exists(rootOutputFolder))
+                Directory.CreateDirectory(rootOutputFolder);
+
+            var imageSizes = new ImageSizes();
+            var f = new FileInfo(fileToResize);
+
+            foreach (var imgSize in imageSizes.Sizes)
+            {
+                using (var fileStream = File.Open(fileToResize, FileMode.Open))
+                {
+                    var outputImage = AzureResize(fileStream, imgSize.Width, imgSize.Height, true );
+                    File.WriteAllBytes(
+                        rootOutputFolder + "/" + Path.GetFileNameWithoutExtension(f.Name) + "_" + imgSize.Width + "x" + imgSize.Height + f.Extension,
+                        outputImage);
+                }
+            }
+
         }
     }
 }
